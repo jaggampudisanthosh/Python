e30401b8ac84a7b106098f3a38cee5a2b65eb3af
JAGGAMPUDI SANTHOSH <jaggampudisanthosh7@gmail.com>
2026-03-09 14:48:09 +0530

    Add files via upload

---
+++ Multipication_of_matrices.py
@@ -0,0 +1,31 @@
+
+r = int(input("Enter number of rows: "))
+c = int(input("Enter number of columns: "))
+
+A = []
+B = []
+
+print("Enter elements of Matrix A:")
+for i in range(r):
+    row = []
+    for j in range(c):
+        row.append(int(input()))
+    A.append(row)
+
+print("Enter elements of Matrix B:")
+for i in range(r):
+    row = []
+    for j in range(c):
+        row.append(int(input()))
+    B.append(row)
+C = [[0 for j in range(c)] for i in range(r)]
+for i in range(r):
+    for j in range(c):
+        for k in range(c):
+            C[i][j] += A[i][k] * B[k][j]
+
+print("Result Matrix:")
+for i in range(r):
+    for j in range(c):
+        print(C[i][j], end=" ")
+    print()
