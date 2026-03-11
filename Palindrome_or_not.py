7d4f2ea31b253395c4aa33ede2619ad5b353cd43
JAGGAMPUDI SANTHOSH <jaggampudisanthosh7@gmail.com>
2026-03-11 07:39:53 +0530

    Add files via upload

---
+++ Palindrome_or_not.py
@@ -0,0 +1,13 @@
+n = int(input("Enter a number: "))
+temp = n
+rev = 0
+
+while n > 0:
+    r = n % 10
+    rev = rev * 10 + r
+    n = n // 10
+
+if temp == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
