1db6acec4c33606c4a2daddf9e93f465e23e3e5b
JAGGAMPUDI SANTHOSH <jaggampudisanthosh7@gmail.com>
2026-03-11 07:41:25 +0530

    Add files via upload

---
+++ Spyno_or_not.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+
+sum = 0
+product = 1
+
+while n > 0:
+    r = n % 10
+    sum = sum + r
+    product = product * r
+    n = n // 10
+
+if sum == product:
+    print("Spy Number")
+else:
+    print("Not Spy Number")
